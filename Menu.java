7f940e05171da7948750f88fde127bee4b9b39ad
Ajmel Muadz <muadzshakir26@gmail.com>
2023-10-17 20:41:31 +0800

    Checkpoint 6: Finished part 3

--- Menu.java
+++ Menu.java
@@ -22,7 +22,7 @@ public class Menu
             System.out.print("\nOperation: ");
             operationChoice = input.nextLine();
 
-            if(operationChoice.equals("1"))
+            if(operationChoice.equals("1"))  // Adding a shop node
             {
                 System.out.println("\n(Adding a shop)");
 
@@ -56,34 +56,34 @@ public class Menu
                     System.out.println("That shop number already exists! Please enter a unique one.");
                 }
             }
-            else if(operationChoice.equals("2"))
+            else if(operationChoice.equals("2"))  // Removing a shop node
             {
                 System.out.println("\n(Removing a shop)");
 
-                System.out.println("Enter shop number to be removed.");
+                System.out.println("\nEnter shop number to be removed.");
                 int shopNumberToRemove = shopNumber(input);
 
                 // Check if shop even exists before removing it
                 if(graph.getVertex(shopNumberToRemove) == null)
                 {
-                    System.out.println("This shop does not even exist! Please add the shop first.");
+                    System.out.println("This shop does not exist! Unable to remove.");
                 }
                 else
                 {
                     graph.removeVertex(shopNumberToRemove);
                 }
             }
-            else if(operationChoice.equals("3"))
+            else if(operationChoice.equals("3"))  // Updating a shop's information
             {
                 System.out.println("\n(Updating a shop's information)");
 
-                System.out.println("Enter shop number to be updated.");
-                int shopNumber = shopNumber(input);
+                System.out.println("\nEnter shop number to be updated.");
+                int shopNumberToUpdate = shopNumber(input);
 
                 // Check if shop even exists before updating it
-                if(graph.getVertex(shopNumber) == null)
+                if(graph.getVertex(shopNumberToUpdate) == null)
                 {
-                    System.out.println("This shop does not even exist! Please add the shop first.");
+                    System.out.println("This shop does not exist! Unable to update.");
                 }
                 else
                 {
@@ -99,40 +99,44 @@ public class Menu
                             case "1":
                                 int updatedShopNumber = shopNumber(input);
                                 // Update the shop number
-                                graph.getVertex(shopNumber).setLabel(updatedShopNumber);
+                                graph.getVertex(shopNumberToUpdate).setLabel(updatedShopNumber);
                                 
                                 break;
 
                             case "2":
                                 String updatedShopName = shopName(input);
                                 // Update the shop name
-                                graph.getVertex(shopNumber).getValue().setName(updatedShopName);
+                                graph.getVertex(shopNumberToUpdate).getValue().setName(updatedShopName);
 
                                 break;
 
                             case "3":
                                 String updatedShopCategory = shopCategory(input);
                                 // Update the shop category
-                                graph.getVertex(shopNumber).getValue().setCategory(updatedShopCategory);
+                                graph.getVertex(shopNumberToUpdate).getValue().setCategory(updatedShopCategory);
 
                                 break;
 
                             case "4":
                                 String updatedShopLocation = shopLocation(input);
                                 // Update the shop location
-                                graph.getVertex(shopNumber).getValue().setLocation(updatedShopLocation);
+                                graph.getVertex(shopNumberToUpdate).getValue().setLocation(updatedShopLocation);
 
                                 break;
 
                             case "5":
                                 int updatedShopRating = shopRating(input);
                                 // Update the shop rating
-                                graph.getVertex(shopNumber).getValue().setRating(updatedShopRating);
+                                graph.getVertex(shopNumberToUpdate).getValue().setRating(updatedShopRating);
 
                                 break;
 
                             case "6":
                                 shopOptionsMenu = false;
+                                break;
+
+                            case "HELP": case "help":
+                                printUpdateShopOptions();
                                 break;
                             
                             default:
@@ -142,13 +146,81 @@ public class Menu
                     }
                 }
             }
-            else if(operationChoice.equals("4"))
+            else if(operationChoice.equals("4"))  // Adding pathways between shops (edges)
+            {
+                System.out.println("\n(Adding pathways between shops)");
+
+                System.out.println("\nEnter the first shop's number.");
+                int firstShop = shopNumber(input);
+
+                // Do not add edges to the first shop if it does not exist.
+                if(graph.getVertex(firstShop) == null)
+                {
+                    System.out.println("First shop does not exist! Unable to add pathway.");
+                }
+                else
+                {
+                    System.out.println("Enter the second shop's number.");
+                    int secondShop = shopNumber(input);
+
+                    // Do not add edges to the second shop if it does not exist
+                    if(graph.getVertex(secondShop) == null)
+                    {
+                        System.out.println("Second shop does not exist! Unable to add pathway.");
+                    }
+                    // If the second shop input is the same as the first shop, don't add edges.
+                    // (two edges cannot be added to the same shop)
+                    else if(secondShop == firstShop)
+                    {
+                        System.out.println("Second shop cannot be the same as the first shop!");
+                    }
+                    else
+                    {
+                        graph.addEdge(firstShop, secondShop);
+                    }
+                }
+            }
+            else if(operationChoice.equals("5")) // Removing pathways between shops (edges)
+            {
+                System.out.println("\n(Removing pathways between shops)");
+
+                System.out.println("\nEnter the first shop's number.");
+                int firstShop = shopNumber(input);
+
+                if(graph.getVertex(firstShop) == null)
+                {
+                    System.out.println("First shop does not exist! Unable to remove edge.");
+                }
+                else
+                {
+                    System.out.println("Enter the second shop's number.");
+                    int secondShop = shopNumber(input);
+
+                    if(graph.getVertex(secondShop) == null)
+                    {
+                        System.out.println("Second shop does not exist! Unable to remove edge.");
+                    }
+                    else if(secondShop == firstShop)
+                    {
+                        System.out.println("Second shop cannot be the same as the first shop!");
+                    }
+                    else
+                    {
+                        graph.removeEdge(firstShop, secondShop);
+                    }
+                }
+            }
+            else if(operationChoice.equals("6"))
             {
                 graph.displayAsList();
             }
-            else if(operationChoice.equals("5"))
+            else if(operationChoice.equals("7"))
             {
                 programRuns = false;
+            }
+            else if(operationChoice.equals("HELP") || operationChoice.equals("help"))
+            {
+                printOptions();
             }
             else
             {
@@ -167,13 +239,17 @@ public class Menu
         System.out.println("1) Add a shop");
         System.out.println("2) Remove a shop");
         System.out.println("3) Update a shop's information");
-        System.out.println("4) Display the list");
-        System.out.println("5) Quit program");
+        System.out.println("4) Add pathways between shops");
+        System.out.println("5) Removing pathways between shops");
+        System.out.println("6) Display the list");
+        System.out.println("7) Quit program");
 
         System.out.println();
 
         System.out.println("Please type the number for your desired operation.");
-        System.out.println("e.g. '1' for adding a shop.");
+        System.out.println("e.g. '1' for adding a shop.\n");
+
+        System.out.println("NOTE: Please type 'HELP' for this same manual to display.");
         System.out.println("------------------------------------------------------------");
     }
 
@@ -194,7 +270,9 @@ public class Menu
         System.out.println();
 
         System.out.println("Please type the number for your desired operation.");
-        System.out.println("e.g. '1' for updating shop number");
+        System.out.println("e.g. '1' for updating shop number.\n");
+
+        System.out.println("NOTE: Please type 'HELP' for this update manual to display.");
         System.out.println("------------------------------------------------------------");
     }
 
